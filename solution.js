1a72b9db4d263b62bb39d2e44077c2ad0dde2bff
Abdullah <Abdullah@Ashwaqs-MacBook-Pro.local>
2019-11-24 01:12:36 +0300

    solved

---
+++ solution.js
@@ -0,0 +1,75 @@
+```
+#1
+
+```
+function sum (N) {
+  var total = 0;
+    for(var i = 1; i <= N; i++){
+      total += i;
+    }
+    console.log (total)
+}
+
+sum(5)
+
+```
+#2
+
+```
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var total = 0;
+
+for ( var i = 0 ; i < arr.length ; i = i + 1)
+{
+	if (arr[i] % 2 == 0)
+	{
+		total += arr[i]
+	}
+}
+
+console.log(total)
+
+### question: when i put var total; it didnt work when i put var total = 0; it worked
+
+```
+#3
+
+```
+
+
+
+```
+#4
+
+```
+
+function reverseString(word) {
+    var newString = "";
+    for (var i = word.length - 1; i >= 0; i--) {
+        newString += word[i];
+    }
+    return newString;
+}
+reverseString('caterpillar');
+
+
+```
+#5
+
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
